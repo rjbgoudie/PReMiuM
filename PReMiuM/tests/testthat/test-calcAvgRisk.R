583895f8c8cb253139a1746592fb99c20b3bedbb
silvialiverani <liveranis@gmail.com>
2017-10-27 16:20:31 +0100

    Improved efficienty of plotRiskProfile

--- PReMiuM/tests/testthat/test-calcAvgRisk.R
+++ PReMiuM/tests/testthat/test-calcAvgRisk.R
@@ -1,15 +1,15 @@
-context("Main profile regression function")
+context("Dissimilarity function")
 
-test_that("MCMC output of main profile regression function runs correctly", {
-
+test_that("Dissimilarity function is running correctly", {
+  library(PReMiuM)
+  library(testthat)
   set.seed(1234)
   generateDataList <- clusSummaryBernoulliDiscrete()
   inputs <- generateSampleDataFile(generateDataList)
   runInfoObj<-profRegr(yModel=inputs$yModel, xModel=inputs$xModel, 
-                       nSweeps=10, nBurn=20, data=inputs$inputData, output="output", 
+                       nSweeps=1, nBurn=0, data=inputs$inputData, output="output", 
                        covNames=inputs$covNames,nClusInit=15, seed=12345)
-  
   dissimObj<-calcDissimilarityMatrix(runInfoObj)
-    
-  expect_equal(dissimObj$disSimMat[3], 0.8)
+  expect_equal(dissimObj$disSimMat[3], 0)
 })
+
